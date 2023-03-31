f91135ec9ea5d203c376e9ac8cabe2ee060f2b55
anton_penkov <anton.penkov1@gmail.com>
2023-03-31 19:21:31 +0200

    working app

--- ButtonAnimation/ContentView.swift
+++ ButtonAnimation/ContentView.swift
@@ -8,12 +8,39 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var isPressed = false
+    @State private var counter = 0
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Text("\(counter)")
+                .font(.system(size: 60, weight: .bold))
+                .opacity(isPressed ? 0.6 : 1)
+                .scaleEffect(isPressed ? 1.5 : 1)
+            
+            ZStack {
+                Capsule()
+                    .frame(width: 150, height: 50)
+                    .foregroundColor(.blue)
+                
+                Text("Tap me")
+                    .font(.system(size: 25, weight: .semibold, design: .rounded))
+                    .foregroundColor(.white)
+            }
+            .opacity(isPressed ? 0.6 : 1)
+            .scaleEffect(isPressed ? 1.3 : 1)
+            .onTapGesture {
+                counter += 1
+            }
+            .pressEvents {
+                withAnimation(.easeIn(duration: 0.1)) {
+                    isPressed = true
+                }
+            } onRelease: {
+                withAnimation {
+                    isPressed = false
+                }
+            }
         }
         .padding()
     }
