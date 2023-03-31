ebba48ddee7963cb92f7ab38c0005a9fac823ef1
anton_penkov <anton.penkov1@gmail.com>
2023-03-31 18:40:40 +0200

    Initial Commit

---
+++ ButtonAnimation/ButtonAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  ButtonAnimationApp.swift
+//  ButtonAnimation
+//
+//  Created by Антон Пеньков on 31.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct ButtonAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
